4f9ad19defe11823e43481fa1b4b8bb001dd9d20
Alessia <a.amore9393@gmail.com>
2022-11-09 11:41:07 +0100

    cart with total added

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 function Basket(props) {
+  function getTotal() {
+    let total = 0;
+    props.cart.forEach((item) => (total += item.price * item.amount));
+    return total;
+  }
   return (
     <section className="Basket">
       <ul>
@@ -10,6 +15,7 @@ function Basket(props) {
           </li>
         ))}
       </ul>
+      <h3>Total: {getTotal()},-</h3>
       <button>Buy now</button>
     </section>
   );
