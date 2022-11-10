c41f0e618defc0be4e061399a94e15b79e66b2a6
Alessia <a.amore9393@gmail.com>
2022-11-10 10:59:32 +0100

    ordering and conditional rendering added

--- src/modules/db.js
+++ src/modules/db.js
@@ -1,8 +1,8 @@
-export function insertOrder(payload) {
+export async function insertOrder(payload) {
   const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqeXpuYWh2dWp4Y2R4cmZsZmJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg3NzUsImV4cCI6MTk4MjE3NDc3NX0.Q_odFvLUcCYUvC8rHHl4HOfIdFZaCHCKuOzo-3B7cuQ";
   const url = "https://jjyznahvujxcdxrflfbc.supabase.co";
 
-  fetch(url + "/rest/v1/mini-shop", {
+  const res = await fetch(url + "/rest/v1/mini-shop", {
     method: "POST",
     headers: {
       apikey: key,
@@ -11,7 +11,6 @@ export function insertOrder(payload) {
       Prefer: " return=representation",
     },
     body: JSON.stringify(payload),
-  })
-    .then()
-    .then();
+  });
+  return await res.json();
 }
